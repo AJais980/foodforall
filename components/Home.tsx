2e8bf7f4256c112d653425517e0efe13304244cc
AJais980 <amanjais980@gmail.com>
2023-09-15 19:01:46 +0530

    final initial

--- components/Home.tsx
+++ components/Home.tsx
@@ -4,36 +4,18 @@ const Home = () => {
     return (
         <div>
             <h1 aria-label="Food For All" className="hero_title mt-10">
-                <span className="animated-gradient-text_background"
-                    style={{
-                        "--content": "'Food'", // Note the single quotes around 'Food'
-                        "--padding": "0.05em",
-                        "--start-color": "var(--food-start-gradient)",
-                        "--end-color": "var(--food-end-gradient)"
-                    }}>
-                    <span className="animated-gradient-text_foreground animated-gradient-text_foreground-1">
+                <span className="animated-gradient-text_background txt-food">
+                    <span className="animated-gradient-text_foreground-1">
                         Food
                     </span>
                 </span>
-                <span className="animated-gradient-text_background"
-                    style={{
-                        "--content": "'For'", // Note the single quotes around 'For'
-                        "--padding": "0.05em",
-                        "--start-color": "var(--for-start-gradient)",
-                        "--end-color": "var(--for-end-gradient)"
-                    }}>
-                    <span className="animated-gradient-text_foreground animated-gradient-text_foreground-2">
+                <span className="animated-gradient-text_background txt-for">
+                    <span className="animated-gradient-text_foreground-2">
                         For
                     </span>
                 </span>
-                <span className="animated-gradient-text_background"
-                    style={{
-                        "--content": "'All'", // Note the single quotes around 'All'
-                        "--padding": "0.05em",
-                        "--start-color": "var(--all-start-gradient)",
-                        "--end-color": "var(--all-end-gradient)"
-                    }}>
-                    <span className="animated-gradient-text_foreground animated-gradient-text_foreground-3">
+                <span className="animated-gradient-text_background txt-all">
+                    <span className="animated-gradient-text_foreground-3">
                         All
                     </span>
                 </span>
