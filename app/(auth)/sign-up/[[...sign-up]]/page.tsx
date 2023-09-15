3314b64a258370b92edcdfb3d4d862e8241eaf21
AJais980 <amanjais980@gmail.com>
2023-09-15 10:21:44 +0530

    initial last

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const Page = () => {
     return (
-        <div className="flex justify-center items-center min-h-screen">
-            <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-lg">
-                <h1 className="text-2xl font-bold text-center mb-4">
+        <div className="flex justify-center items-center min-h-screen ">
+            <div className="w-full max-w-md bg-base-200 bg-opacity-25 filter backdrop-filter backdrop-blur-lg bg-transparent rounded-lg shadow-lg">
+                <h1 className="text-2xl font-bold text-center px-4 py-4">
                     Sign Up to Access the Full Features of the Website
                 </h1>
                 <SignUp />
