c0e639f819da0a14449ef4acf678014588ac68f1
AJais980 <amanjais980@gmail.com>
2023-10-26 21:54:55 +0530

    sign-in/up layout changes

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Page = () => {
     return (
         <div className="flex justify-center items-center min-h-screen ">
-            <div className="w-full max-w-md bg-base-200 bg-opacity-25 filter backdrop-filter backdrop-blur-lg bg-transparent rounded-lg shadow-lg">
+            <div className="flex flex-col items-center justify-center pb-6 mt-12 mb-12 w-full max-w-md bg-base-200 bg-opacity-25 filter backdrop-filter backdrop-blur-lg bg-transparent rounded-lg shadow-lg">
                 <h1 className="text-2xl font-bold text-center px-4 py-4">
                     Sign Up to Access the Full Features of the Website
                 </h1>
