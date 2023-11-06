d430adda4cf3a117f55262c99e8b749bc02b3dfa
AJais980 <amanjais980@gmail.com>
2023-11-06 22:27:48 +0530

    update in inventory API

--- app/api/inventory/route.ts
+++ app/api/inventory/route.ts
@@ -1,23 +1,4 @@
-import UserModel from "@/lib/models/User";
 import { getUser } from "@/lib/utils";
-import { NextRequest } from "next/server";
-
-export async function POST(req: NextRequest) {
-    try {
-        const user = await getUser();
-        const itemsToAdd = await req.json();
-
-        const dbUser = await UserModel.findOne({ id: user.id });
-
-        dbUser.foodItems = [...itemsToAdd];
-        await dbUser.save();
-
-        return new Response(JSON.stringify(dbUser.foodItems));
-    } catch (error) {
-        console.error("POST error:", error);
-        return new Response("An error occurred.", { status: 500 });
-    }
-}
 
 export async function GET() {
     try {
@@ -25,25 +6,6 @@ export async function GET() {
         return new Response(JSON.stringify(user?.addedToCart));
     } catch (error) {
         console.error("GET error:", error);
-        return new Response("An error occurred.", { status: 500 });
-    }
-}
-
-export async function DELETE(req: NextRequest) {
-    try {
-        const user = await getUser();
-        const itemsToRemove = await req.json();
-
-        const dbUser = await UserModel.findOne({ id: user.id });
-
-        dbUser.foodItems = dbUser.foodItems.filter(
-            (item: any) => !itemsToRemove.includes(item)
-        );
-        await dbUser.save();
-
-        return new Response(JSON.stringify(dbUser.foodItems));
-    } catch (error) {
-        console.error("DELETE error:", error);
         return new Response("An error occurred.", { status: 500 });
     }
 }
