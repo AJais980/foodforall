1f7a8d2a64f1483ffe3eb9a58c6fe39885ed9645
AJais980 <amanjais980@gmail.com>
2023-11-06 21:55:04 +0530

    test 5 for cart API

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -2,12 +2,17 @@ import { getUser } from "@/lib/utils";
 
 export async function GET() {
     try {
-        const user = getUser();
-        console.log("Trying...\n");
-        console.log("User: ", user);
+        let user = await tryGetUser();
         return new Response(JSON.stringify(user));
     } catch (error) {
         console.error("GET error:", error);
         return new Response("An error occurred.", { status: 500 });
     }
+}
+
+async function tryGetUser() {
+    let user = await getUser();
+    if (!user)
+        tryGetUser();
+    return user;
 }
